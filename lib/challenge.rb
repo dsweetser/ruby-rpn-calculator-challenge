56a59b99ce601ba91835ee33c1a7d492e0bf7c98
dsweetser <daniel.sweetser@gmail.com>
2017-03-14 18:57:03 -0400

    Challenge
    Attempted challenge.  Had basic addition and subtraction working at end,
    but not advanced addition and subtraction (which I think would be done
    with .slice).  However, code is currently broken as I tried to implement
    * and / which were causing issues for some reason.

--- lib/challenge.rb
+++ lib/challenge.rb
@@ -1,4 +1,32 @@
 # frozen_string_literal: true
 def calc(expr)
-  # Your code here
+  expr_array = expr.split(' ')
+  puts expr_array
+  if expr_array.last == +
+    expr_array.pop
+    total = expr_array[0].to_i
+    expr_array.shift
+    expr_array.each { |x| total += x.to_i }
+    total
+  elsif expr_array.last == -
+    expr_array.pop
+    total = expr_array[0].to_i
+    expr_array.shift
+    expr_array.each { |x| total -= x.to_i }
+    total
+  elsif expr_array.last == '*'
+    expr_array.pop
+    total = expr_array[0].to_i
+    expr_array.shift
+    expr_array.each { |x| total *= x.to_i }
+    total
+  elsif expr_array.last == /
+    expr_array.pop
+    total = expr_array[0].to_i
+    expr_array.shift
+    expr_array.each { |x| total /= x.to_i }
+    total
+  else
+    0
+  end
 end
